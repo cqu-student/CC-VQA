c95960a0bceeaaadd8f470aa387271c12577f2b3
Hoya <68796008+cqu-student@users.noreply.github.com>
2026-04-01 21:15:33 +0800

    Add test script for accelerate launch configuration

---
+++ test/test.sh
@@ -0,0 +1,18 @@
+accelerate launch --num_processes 8 --mixed_precision bf16 test_retrieval_accelerate_nj_coco_with_similarity.py   \
+  --fusion_mode dsab \
+  --dsab_stage pre_warpers \
+  --dsab_mix_space logprob \
+  --dsab_alpha_gamma 1.0 \
+  --dsab_alpha_use_d2 \
+  --dsab_use_safeguards \
+  --dsab_lite \
+  --dsab_alpha_min 0.1 \
+  --dsab_alpha_max 0.9 \
+
+  
+  # --sim_mode alpha_bias \
+  # --sim_beta 0.1 \
+  # --sim_center 0.5 \
+  # --sim_mode alpha \
+  # --sim_pool max \
+  # --sim_alpha_weight 0.8
